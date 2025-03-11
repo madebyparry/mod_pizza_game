f3ae53ab013ca01192a12b97c5209fa6a27d5259
MadeByParry <nicholas@madebyparry.com>
2025-03-11 00:59:20 +0000

    style and server config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
-import pizzaJson from './assets/pizzalist.json'
-import ingredientsJson from './assets/ingredientslist.json'
-import './assets/common.scss'
-import './App.css'
+import { useState, useEffect } from "react";
+import pizzaJson from "./assets/pizzalist.json";
+import ingredientsJson from "./assets/ingredientslist.json";
+import "./assets/common.scss";
+import "./App.css";
 
 function App() {
   const [currentIngredients, setCurrentIngredients] = useState([]);
@@ -10,48 +10,52 @@ function App() {
   let ingredientsCategories = Object.keys(ingredientsJson);
 
   function shuffle(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
   const [shuffledPizzas, setShuffledPizzas] = useState(shuffle(pizzaList));
   const [pizzaProgress, setPizzaProgress] = useState(0);
-  const [currentPizza, setCurrentPizza] = useState(shuffledPizzas[pizzaProgress]);
+  const [currentPizza, setCurrentPizza] = useState(
+    shuffledPizzas[pizzaProgress],
+  );
   const [lastResult, setLastResult] = useState(null);
 
   useEffect(() => {
-    console.log('shuffled pizzas',shuffledPizzas);
+    console.log("shuffled pizzas", shuffledPizzas);
     console.log(pizzaProgress);
     console.log(currentPizza);
     setCurrentPizza(shuffledPizzas[pizzaProgress]);
-  },[pizzaProgress]);
+  }, [pizzaProgress]);
 
   function ingredientHandler(ingredient) {
-      if (currentIngredients.includes(ingredient)) {
-        const cI = currentIngredients.filter(item => item !== ingredient);
-        setCurrentIngredients(cI);
-        console.log(cI);
-        console.log('removed', ingredient);
-      } else {
-        setCurrentIngredients((prev) => [...prev, ingredient]);
-        console.log('added', ingredient);
-      }
+    if (currentIngredients.includes(ingredient)) {
+      const cI = currentIngredients.filter((item) => item !== ingredient);
+      setCurrentIngredients(cI);
+      console.log(cI);
+      console.log("removed", ingredient);
+    } else {
+      setCurrentIngredients((prev) => [...prev, ingredient]);
+      console.log("added", ingredient);
+    }
   }
   function submitHandler() {
     function isEqual(a, b) {
-      return a.length === b.length &&
-          a.every(element => b.includes(element)) &&
-          b.every(element => a.includes(element));
+      return (
+        a.length === b.length &&
+        a.every((element) => b.includes(element)) &&
+        b.every((element) => a.includes(element))
+      );
     }
-    console.log('submit');
+    console.log("submit");
     if (isEqual(currentIngredients, pizzaJson[currentPizza])) {
-      setLastResult('match');
-      console.log('match');
+      setLastResult("match");
+      console.log("match");
     } else {
-      setLastResult('no match');
-      console.log('no match');
+      setLastResult("no match");
+      console.log("no match");
     }
     setCurrentIngredients([]);
     let p = pizzaProgress + 1;
@@ -60,50 +64,56 @@ function App() {
 
   return (
     <>
-      <h1>MOD</h1>
-      <h2>{currentPizza}</h2>
       {lastResult ? (
-        <div>
-          <h4>Last Pizza: {shuffledPizzas[pizzaProgress - 1]}</h4>
-          <h5>{lastResult}</h5>
+        <div className={`last-pizza ${lastResult.replace(" ", "-")}`}>
+          <p>Last Pizza: {shuffledPizzas[pizzaProgress - 1]}</p>
+          <p>{lastResult}</p>
         </div>
-      ) : null}
+      ) : (
+        <h1>MOD</h1>
+      )}
+      <hr />
+      <h2>{currentPizza}</h2>
       <div className="current-ingredients">
         <ul className="current-ingredients-list">
-          {
-            currentIngredients.map((ingredient)=>{
-              return (
-                <li>{ingredient}</li>
-              );
-            })
-          }
+          {currentIngredients.map((ingredient) => {
+            return <li>{ingredient}</li>;
+          })}
         </ul>
-        <button onClick={() => {submitHandler()}}>SUBMIT</button>
+        <button
+          onClick={() => {
+            submitHandler();
+          }}
+        >
+          SUBMIT
+        </button>
       </div>
-      <hr/>
+      <hr />
       <div className="ingredients-wrapper">
-        {
-          ingredientsCategories.map((item)=>{
-            return (
-              <div className="ingredients-column">
-              {
-                ingredientsJson[item].map((ingredient)=>{
-                  return (
-                    <button 
-                      value={ingredient}
-                      onClick={()=>{ingredientHandler(ingredient)}}
-                      className={currentIngredients.includes(ingredient) ? 'active' : ''}
-                    >{ingredient}</button>
-                  )
-                })
-              }
-              </div>
-            )
-          })
-        }
+        {ingredientsCategories.map((item) => {
+          return (
+            <div className="ingredients-column">
+              {ingredientsJson[item].map((ingredient) => {
+                return (
+                  <button
+                    value={ingredient}
+                    onClick={() => {
+                      ingredientHandler(ingredient);
+                    }}
+                    className={
+                      currentIngredients.includes(ingredient) ? "active" : ""
+                    }
+                  >
+                    {ingredient}
+                  </button>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/mod_pizza_game/dist",
+  build: {
+    outDir: "/var/www/html/mod_pizza_game/dist/",
+    emptyOutDir: true,
+    assetDir: "/var/www/html/mod_pizza_game/dist/assets/",
+    manifest: "manifest.json",
+    sourcemap: true,
+  },
+});
